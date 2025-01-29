8cda32051b611ba80db7ffda36d843fe2ab9e08b
JaredAcord <jaredacord@gmail.com>
2025-01-29 12:18:00 -0800

    Redid test cases to match report

--- src/test/java/org/jabref/SWE261P_tests/assignment1_tests.java
+++ src/test/java/org/jabref/SWE261P_tests/assignment1_tests.java
@@ -16,200 +16,161 @@ public class assignment1_tests {
     private BibEntry entry = new BibEntry();
 
     @Test
-    void checkEntryDateValidReasonable() {
-        entry.setField(StandardField.DATE, "1984-12-16");
-        assertEquals(Optional.of("1984-12-16"), entry.getFieldOrAlias(StandardField.DATE));
-        isYearMonthDayEqualTo(Optional.of("1984"), Optional.of("#dec#"), Optional.of("16"));
-    }
-
-    @Test
-    void checkEntryDateValidWithWhitespece() {
-        entry.setField(StandardField.DATE, "1984 - 12 - 16");
-        assertEquals(Optional.of("1984 - 12 - 16"), entry.getFieldOrAlias(StandardField.DATE));
-        isYearMonthDayEmpty();
-    }
-
-    @Test
-    void checkEntryDateValidNoDay() {
+    void checkEntryDateDDEmpty() {
         entry.setField(StandardField.DATE, "1984-12");
         assertEquals(Optional.of("1984-12"), entry.getFieldOrAlias(StandardField.DATE));
         isYearMonthDayEqualTo(Optional.of("1984"), Optional.of("#dec#"), Optional.empty());
     }
 
     @Test
-    void checkEntryDateValidNoDayNoMonth() {
+    void checkEntryDateDDNonNum() {
+        entry.setField(StandardField.DATE, "1984-12-a9");
+        assertEquals(Optional.of("1984-12-a9"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEmpty();
+        entry.setField(StandardField.DATE, "1984-12-5h&8K");
+        assertEquals(Optional.of("1984-12-5h&8K"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEmpty();
+    }
+
+    @Test
+    void checkEntryDateDDLessThan1() {
+        entry.setField(StandardField.DATE, "1984-12-0");
+        assertEquals(Optional.of("1984-12-0"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEmpty();
+        entry.setField(StandardField.DATE, "1984-12--1");
+        assertEquals(Optional.of("1984-12--1"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEmpty();
+    }
+
+    @Test
+    void checkEntryDateDDValidUnder10With0() {
+        entry.setField(StandardField.DATE, "1984-12-05");
+        assertEquals(Optional.of("1984-12-05"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEqualTo(Optional.of("1984"), Optional.of("#dec#"), Optional.of("5"));
+    }
+
+    @Test
+    void checkEntryDateDDValidUnder10No0() {
+        entry.setField(StandardField.DATE, "1984-12-5");
+        assertEquals(Optional.of("1984-12-5"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEqualTo(Optional.of("1984"), Optional.of("#dec#"), Optional.of("5"));
+    }
+
+    @Test
+    void checkEntryDateDDValidDoubleDigit() {
+        entry.setField(StandardField.DATE, "1984-12-21");
+        assertEquals(Optional.of("1984-12-21"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEqualTo(Optional.of("1984"), Optional.of("#dec#"), Optional.of("21"));
+    }
+
+    @Test
+    void checkEntryDateDDGreaterThan31() {
+        entry.setField(StandardField.DATE, "1984-12-32");
+        assertEquals(Optional.of("1984-12-32"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEmpty();
+    }
+
+    @Test
+    void checkEntryDateMMEmpty() {
         entry.setField(StandardField.DATE, "1984");
         assertEquals(Optional.of("1984"), entry.getFieldOrAlias(StandardField.DATE));
         isYearMonthDayEqualTo(Optional.of("1984"), Optional.empty(), Optional.empty());
     }
 
     @Test
-    void checkEntryDateWithDateNonNumImproperFormat() {
-        entry.setField(StandardField.DATE, "abcabc");
-        assertEquals(Optional.of("abcabc"), entry.getFieldOrAlias(StandardField.DATE));
+    void checkEntryDateMMNonNum() {
+        entry.setField(StandardField.DATE, "2005-A1-12");
+        assertEquals(Optional.of("2005-A1-12"), entry.getFieldOrAlias(StandardField.DATE));
         isYearMonthDayEmpty();
-        entry.setField(StandardField.DATE, "&$#*8HDUu");
-        assertEquals(Optional.of("&$#*8HDUu"), entry.getFieldOrAlias(StandardField.DATE));
-        isYearMonthDayEmpty();
-    }
-
-    @Test
-    void checkEntryDateInvalidDate() {
-        entry.setField(StandardField.DATE, "2024-02-30");
-        assertEquals(Optional.of("2024-02-30"), entry.getFieldOrAlias(StandardField.DATE));
+        entry.setField(StandardField.DATE, "2005-W#4f*-12");
+        assertEquals(Optional.of("2005-W#4f*-12"), entry.getFieldOrAlias(StandardField.DATE));
         isYearMonthDayEmpty();
     }
 
     @Test
-    void checkEntryDateWithYearTripleDigit() {
-        entry.setField(StandardField.DATE, "999-12-20");
-        assertEquals(Optional.of("999-12-20"), entry.getFieldOrAlias(StandardField.DATE));
+    void checkEntryDateMMLessThan1() {
+        entry.setField(StandardField.DATE, "1984-0-10");
+        assertEquals(Optional.of("1984-0-10"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEmpty();
+        entry.setField(StandardField.DATE, "1984--1-10");
+        assertEquals(Optional.of("1984--1-10"), entry.getFieldOrAlias(StandardField.DATE));
         isYearMonthDayEmpty();
     }
 
     @Test
-    void checkEntryDateWithYearZero() {
-        entry.setField(StandardField.DATE, "0-10-16");
-        assertEquals(Optional.of("0-10-16"), entry.getFieldOrAlias(StandardField.DATE));
+    void checkEntryDateMMValidUnder10With0() {
+        entry.setField(StandardField.DATE, "2022-03");
+        assertEquals(Optional.of("2022-03"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEqualTo(Optional.of("2022"), Optional.of("#mar#"), Optional.empty());
+    }
+
+    @Test
+    void checkEntryDateMMValidUnder10No0() {
+        entry.setField(StandardField.DATE, "2022-3-21");
+        assertEquals(Optional.of("2022-3-21"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEqualTo(Optional.of("2022"), Optional.of("#mar#"), Optional.of("21"));
+    }
+
+    @Test
+    void checkEntryDateMMValidDoubleDigit() {
+        entry.setField(StandardField.DATE, "1984-11");
+        assertEquals(Optional.of("1984-11"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEqualTo(Optional.of("1984"), Optional.of("#nov#"), Optional.empty());
+    }
+
+    @Test
+    void checkEntryDateMMGreaterThan12() {
+        entry.setField(StandardField.DATE, "1984-13-28");
+        assertEquals(Optional.of("1984-13-28"), entry.getFieldOrAlias(StandardField.DATE));
         isYearMonthDayEmpty();
     }
 
     @Test
-    void checkEntryDateWithYearNegative() {
-        entry.setField(StandardField.DATE, "-500-10-16");
-        assertEquals(Optional.of("-500-10-16"), entry.getFieldOrAlias(StandardField.DATE));
+    void checkEntryDateYYYYEmpty() {
+        entry.setField(StandardField.DATE, "");
+        assertEquals(Optional.empty(), entry.getFieldOrAlias(StandardField.DATE));
         isYearMonthDayEmpty();
     }
 
     @Test
-    void checkEntryDateWithYearFractional() {
-        entry.setField(StandardField.DATE, "2024.5-02-30");
-        assertEquals(Optional.of("2024.5-02-30"), entry.getFieldOrAlias(StandardField.DATE));
-        this.isYearMonthDayEmpty();
-    }
-
-    @Test
-    void checkEntryDateWithYearNonNum() {
-        entry.setField(StandardField.DATE, "abCD-10-To");
-        assertEquals(Optional.of("abCD-10-To"), entry.getFieldOrAlias(StandardField.DATE));
+    void checkEntryDateYYYYNonNum() {
+        entry.setField(StandardField.DATE, "W#4f*-12-21");
+        assertEquals(Optional.of("W#4f*-12-21"), entry.getFieldOrAlias(StandardField.DATE));
         isYearMonthDayEmpty();
-        entry.setField(StandardField.DATE, "5!@3-10-$");
-        assertEquals(Optional.of("5!@3-10-$"), entry.getFieldOrAlias(StandardField.DATE));
+        entry.setField(StandardField.DATE, "h6&klt");
+        assertEquals(Optional.of("h6&klt"), entry.getFieldOrAlias(StandardField.DATE));
         isYearMonthDayEmpty();
     }
 
     @Test
-    void checkEntryDateWithMonthNonNum() {
-        entry.setField(StandardField.DATE, "1984-A9-16");
-        assertEquals(Optional.of("1984-A9-16"), entry.getFieldOrAlias(StandardField.DATE));
+    void checkEntryDateYYYYLessThan1000() {
+        entry.setField(StandardField.DATE, "-98");
+        assertEquals(Optional.of("-98"), entry.getFieldOrAlias(StandardField.DATE));
         isYearMonthDayEmpty();
-        entry.setField(StandardField.DATE, "1984-#-16");
-        assertEquals(Optional.of("1984-#-16"), entry.getFieldOrAlias(StandardField.DATE));
-        isYearMonthDayEmpty();
-    }
-
-    @Test
-    void checkEntryDateWithMonthZeroPadded() {
-        entry.setField(StandardField.DATE, "1984-09-16");
-        assertEquals(Optional.of("1984-09-16"), entry.getFieldOrAlias(StandardField.DATE));
-        isYearMonthDayEqualTo(Optional.of("1984"), Optional.of("#sep#"), Optional.of("16"));
-    }
-
-    @Test
-    void checkEntryDateWithMonthZeroUnpadded() {
-        entry.setField(StandardField.DATE, "1984-9-16");
-        assertEquals(Optional.of("1984-9-16"), entry.getFieldOrAlias(StandardField.DATE));
-        isYearMonthDayEqualTo(Optional.of("1984"), Optional.of("#sep#"), Optional.of("16"));
-    }
-
-    @Test
-    void checkEntryDateWithMonthZero() {
-        entry.setField(StandardField.DATE, "2015-0-22");
-        assertEquals(Optional.of("2015-0-22"), entry.getFieldOrAlias(StandardField.DATE));
-        this.isYearMonthDayEmpty();
-    }
-
-    @Test
-    void checkEntryDateWithMonthNegative() {
-        entry.setField(StandardField.DATE, "2015--1-22");
-        assertEquals(Optional.of("2015--1-22"), entry.getFieldOrAlias(StandardField.DATE));
-        this.isYearMonthDayEmpty();
-    }
-
-    @Test
-    void checkEntryDateWithMonthGreaterThan12() {
-        entry.setField(StandardField.DATE, "2005-13-21");
-        assertEquals(Optional.of("2005-13-21"), entry.getFieldOrAlias(StandardField.DATE));
-        this.isYearMonthDayEmpty();
-    }
-
-    @Test
-    void checkEntryDateWithMonthFractional() {
-        entry.setField(StandardField.DATE, "1999-0.5-18");
-        assertEquals(Optional.of("1999-0.5-18"), entry.getFieldOrAlias(StandardField.DATE));
-        this.isYearMonthDayEmpty();
-    }
-
-    @Test
-    void checkEntryDateWithMonthString() {
-        entry.setField(StandardField.DATE, "1989-#may#-16");
-        assertEquals(Optional.of("1989-#may#-16"), entry.getFieldOrAlias(StandardField.DATE));
-        this.isYearMonthDayEmpty();
-        entry.setField(StandardField.DATE, "1989-may-16");
-        assertEquals(Optional.of("1989-may-16"), entry.getFieldOrAlias(StandardField.DATE));
-        this.isYearMonthDayEmpty();
-    }
-
-    @Test
-    void checkEntryDateWithDayNonNum() {
-        entry.setField(StandardField.DATE, "1984-10-To");
-        assertEquals(Optional.of("1984-10-To"), entry.getFieldOrAlias(StandardField.DATE));
-        isYearMonthDayEmpty();
-        entry.setField(StandardField.DATE, "1984-10-$");
-        assertEquals(Optional.of("1984-10-$"), entry.getFieldOrAlias(StandardField.DATE));
+        entry.setField(StandardField.DATE, "999-12-1");
+        assertEquals(Optional.of("999-12-1"), entry.getFieldOrAlias(StandardField.DATE));
         isYearMonthDayEmpty();
     }
 
     @Test
-    void checkEntryDateWithDayZeroPadded() {
-        entry.setField(StandardField.DATE, "1984-09-05");
-        assertEquals(Optional.of("1984-09-05"), entry.getFieldOrAlias(StandardField.DATE));
-        isYearMonthDayEqualTo(Optional.of("1984"), Optional.of("#sep#"), Optional.of("5"));
+    void checkEntryDateHolisticValidDate() {
+        entry.setField(StandardField.DATE, "2005-11-22");
+        assertEquals(Optional.of("2005-11-22"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEqualTo(Optional.of("2005"), Optional.of("#nov#"), Optional.of("22"));
     }
 
     @Test
-    void checkEntryDateWithDayZeroUnpadded() {
-        entry.setField(StandardField.DATE, "1984-09-5");
-        assertEquals(Optional.of("1984-09-5"), entry.getFieldOrAlias(StandardField.DATE));
-        isYearMonthDayEqualTo(Optional.of("1984"), Optional.of("#sep#"), Optional.of("5"));
+    void checkEntryDateHolisticNonExistentDate() {
+        entry.setField(StandardField.DATE, "1984-02-30");
+        assertEquals(Optional.of("1984-02-30"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEmpty();
     }
 
     @Test
-    void checkEntryDateWithDayZero() {
-        entry.setField(StandardField.DATE, "2015-10-0");
-        assertEquals(Optional.of("2015-10-0"), entry.getFieldOrAlias(StandardField.DATE));
-        this.isYearMonthDayEmpty();
-    }
-
-    @Test
-    void checkEntryDateWithDayNegative() {
-        entry.setField(StandardField.DATE, "2015-10--1");
-        assertEquals(Optional.of("2015-10--1"), entry.getFieldOrAlias(StandardField.DATE));
-        this.isYearMonthDayEmpty();
-    }
-
-    @Test
-    void checkEntryDateWithDayGreaterThan31() {
-        entry.setField(StandardField.DATE, "2024-05-32");
-        assertEquals(Optional.of("2024-05-32"), entry.getFieldOrAlias(StandardField.DATE));
-        this.isYearMonthDayEmpty();
-    }
-
-    @Test
-    void checkEntryDateWithDayFractional() {
-        entry.setField(StandardField.DATE, "1999-11-.5");
-        assertEquals(Optional.of("1999-11-.5"), entry.getFieldOrAlias(StandardField.DATE));
-        this.isYearMonthDayEmpty();
+    void checkEntryDateHolisticFutureDate() {
+        entry.setField(StandardField.DATE, "2050-01-28");
+        assertEquals(Optional.of("2050-01-28"), entry.getFieldOrAlias(StandardField.DATE));
+        isYearMonthDayEqualTo(Optional.of("2050"), Optional.of("#jan#"), Optional.of("28"));
     }
 
     void isYearMonthDayEqualTo(Optional op_year, Optional op_mon, Optional op_day) {
@@ -219,8 +180,8 @@ public class assignment1_tests {
     }
 
     void isYearMonthDayEmpty() {
-        assertEquals(Optional.empty(), entry.getFieldOrAlias(StandardField.YEAR));
-        assertEquals(Optional.empty(), entry.getFieldOrAlias(StandardField.MONTH));
         assertEquals(Optional.empty(), entry.getFieldOrAlias(StandardField.DAY));
+        assertEquals(Optional.empty(), entry.getFieldOrAlias(StandardField.MONTH));
+        assertEquals(Optional.empty(), entry.getFieldOrAlias(StandardField.YEAR));
     }
 }
